96da27695887c1266be5f31f73342c1a77cc76ed
KCCHDEV | Lunaᴾᴿᴼ Developer's Team <golfdx34@gmail.com>
2022-08-12 10:00:10 +0700

    Create start.sh

---
+++ Websitefix/start.sh
@@ -0,0 +1,9 @@
+#!/bin/ash
+
+echo "Starting PHP-FPM..."
+/usr/sbin/php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
+
+echo "Starting Nginx..."
+/usr/sbin/nginx -c /home/container/nginx/nginx.conf
+
+echo "Starting Wordpress..."
